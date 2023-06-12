276d9f8a5f7f9aed92dcb688c12670bc25edb450
Wesley Almeida <72157464+wesleyinfobr@users.noreply.github.com>
2023-06-12 18:40:28 -0300

    Add files via upload

---
+++ dashboard.py
@@ -0,0 +1,11 @@
+# importar o pacote no script para começar a usar
+import streamlit as st  #por convenção vamos apelidar o streamlit de st
+ 
+#Aqui definimos o título da página e o layout como wide
+st.set_page_config(page_title="Meu Dashboard",layout="wide") 
+ 
+#Título do seu dashboard
+st.write("""
+# Meu primeiro Dashboard
+Abaixo veremos os próximos passos
+""") 
